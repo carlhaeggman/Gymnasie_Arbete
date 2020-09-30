1c4883059fae6c67ded64f735967456b3813f0c1
carlhaeggman <carl.haeggman02@gmail.com>
2020-09-30 08:39:52 +0200

    Satt upp scene och börjat movement

--- GymnaiseArbete/Assets/PlayerMovement.cs
+++ GymnaiseArbete/Assets/PlayerMovement.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float speed;
+    Vector3 cameraPosition;
+    public Camera mainCamera;
+
+    float basespeed;
+    public float currentSpeed;
     public Rigidbody rb;
     private float horizontalX;
     private float horizontalY;
 
+    public static float globalGravity = -9.82f;
+    public float gravityScale = 1.0f;
 
-    // Start is called before the first frame update
+    public Transform cameraHolder;
+
+    public AnimationCurve slideValue;
+    private float slideDistance;
+    public float slideMultiplier;
+    public bool canSlide;
+    
     void Start()
     {
-        
+        currentSpeed = 500;
+        basespeed = 500;
+        canSlide = true;
     }
 
     // Update is called once per frame
@@ -23,11 +37,43 @@ public class PlayerMovement : MonoBehaviour
         Movement();
     }
 
+  
     void Movement()
     {
         horizontalX = Input.GetAxisRaw("Horizontal");
         horizontalY = Input.GetAxisRaw("Vertical");
-        var movementDirection = rb.velocity = new Vector3(horizontalX, 0, horizontalY) * speed * Time.deltaTime;
-     
+
+        rb.AddRelativeForce(Time.deltaTime * horizontalX * currentSpeed, 0 * Time.deltaTime, Time.deltaTime * horizontalY * currentSpeed, ForceMode.VelocityChange);
+        rb.velocity = new Vector3(0, rb.velocity.y, 0);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = 800f;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            currentSpeed = 500;
+        }
+
+        if (Input.GetKey(KeyCode.C) && canSlide == true && horizontalY > 0)
+        {
+            MovementSlide();
+            canSlide = false;
+         
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            canSlide = true;
+            currentSpeed = basespeed;
+        }
+        
+    }
+
+    void MovementSlide()
+    {
+            slideDistance = Mathf.Lerp(1, 0.2f, Time.deltaTime * 2);
+            currentSpeed = basespeed * (slideValue.Evaluate(slideDistance) * slideMultiplier);
     }
 }
+
+
