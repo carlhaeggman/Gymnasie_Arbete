e2ef0831656599b431078d2c643286c14e09fffe
carlhaeggman <carl.haeggman02@gmail.com>
2020-11-11 09:47:09 +0100

    Slide

--- GymnaiseArbete/Assets/PlayerMovement.cs
+++ GymnaiseArbete/Assets/PlayerMovement.cs
@@ -8,27 +8,40 @@ public class PlayerMovement : MonoBehaviour
     Vector3 cameraPosition;
     public Camera mainCamera;
 
-    float basespeed;
     public float currentSpeed;
     public Rigidbody rb;
     private float horizontalX;
     private float horizontalY;
+    bool canMove;
 
     public static float globalGravity = -9.82f;
     public float gravityScale = 1.0f;
 
     public Transform cameraHolder;
 
-    public AnimationCurve slideValue;
-    private float slideDistance;
-    public float slideMultiplier;
-    public bool canSlide;
+    Rigidbody rig;
+    CapsuleCollider coll;
+    float originalHeight;
+    public float reducedHeight;
+    public float slideLength;
+
+    public float mouseSensitivity;
+    private float xRotation;
+
+
     
     void Start()
     {
+        slideLength = 0.05f;
+        coll = GetComponent<CapsuleCollider>();
+        rig = GetComponent<Rigidbody>();
+        originalHeight = coll.height;
+
+        canMove = true;
         currentSpeed = 500;
-        basespeed = 500;
-        canSlide = true;
+        mouseSensitivity = 200f;
+        xRotation = 0f;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -36,43 +49,75 @@ public class PlayerMovement : MonoBehaviour
     {
         Movement();
     }
-
-  
-    void Movement()
+    void Update()
     {
-        horizontalX = Input.GetAxisRaw("Horizontal");
-        horizontalY = Input.GetAxisRaw("Vertical");
-
-        rb.AddRelativeForce(Time.deltaTime * horizontalX * currentSpeed, 0 * Time.deltaTime, Time.deltaTime * horizontalY * currentSpeed, ForceMode.VelocityChange);
-        rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("sup");
+            Sliding();
+            canMove = false;
+        }
+        else if (Input.GetKeyUp(KeyCode.C))
+        {
+            GoUp();
+            canMove = true;
+            
+        }
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            currentSpeed = 800f;
+            currentSpeed = 900f;
+            slideLength = 0.25f;
         }
         else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             currentSpeed = 500;
+            slideLength = 0.05f;
         }
 
-        if (Input.GetKey(KeyCode.C) && canSlide == true && horizontalY > 0)
-        {
-            MovementSlide();
-            canSlide = false;
-         
-        }
-        if (Input.GetKeyUp(KeyCode.C))
-        {
-            canSlide = true;
-            currentSpeed = basespeed;
-        }
-        
+        LookAround();
     }
 
-    void MovementSlide()
+
+    void Movement()
     {
-            slideDistance = Mathf.Lerp(1, 0.2f, Time.deltaTime * 2);
-            currentSpeed = basespeed * (slideValue.Evaluate(slideDistance) * slideMultiplier);
+        if(canMove == true)
+        {
+            horizontalX = Input.GetAxisRaw("Horizontal");
+            horizontalY = Input.GetAxisRaw("Vertical");
+
+            rb.AddRelativeForce(Time.deltaTime * horizontalX * currentSpeed, 0 * Time.deltaTime, Time.deltaTime * horizontalY * currentSpeed, ForceMode.VelocityChange);
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+    }
+
+    private void Sliding()
+    {
+
+        coll.height = reducedHeight;
+        rig.AddForce(transform.forward * slideLength, ForceMode.VelocityChange);
+
+    }
+
+    private void GoUp()
+    {
+        coll.height = Mathf.Lerp(coll.height, originalHeight, Time.time/3);
+    }
+
+    private void LookAround()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        cameraHolder.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
 
