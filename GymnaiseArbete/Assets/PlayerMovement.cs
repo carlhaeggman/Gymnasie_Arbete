4ca20df804490294faf2cb1552fbe903848e5598
carlhaeggman <carl.haeggman02@gmail.com>
2020-11-11 08:37:20 +0100

    Update PlayerMovement.cs

--- GymnaiseArbete/Assets/PlayerMovement.cs
+++ GymnaiseArbete/Assets/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float t;
+    private bool timeStarted = false;
+    private float timeElapsed;
 
     Vector3 cameraPosition;
     public Camera mainCamera;
@@ -19,25 +22,27 @@ public class PlayerMovement : MonoBehaviour
 
     public Transform cameraHolder;
 
-    public AnimationCurve slideValue;
-    private float slideDistance;
-    public float slideMultiplier;
-    public bool canSlide;
+    private float slideTime;
+    private bool canSlide;
     
     void Start()
     {
         currentSpeed = 500;
         basespeed = 500;
-        canSlide = true;
+        slideTime = 3f;
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        MovementSlide();
+    }
+
     void FixedUpdate()
     {
         Movement();
     }
 
-  
     void Movement()
     {
         horizontalX = Input.GetAxisRaw("Horizontal");
@@ -54,26 +59,46 @@ public class PlayerMovement : MonoBehaviour
         {
             currentSpeed = 500;
         }
+    }
 
-        if (Input.GetKey(KeyCode.C) && canSlide == true && horizontalY > 0)
+    void MovementSlide()
+    {
+
+        if (!timeStarted)
         {
-            MovementSlide();
+            timeElapsed = Time.time;
+        }
+        float t =+ Time.time - timeElapsed;
+        if (Input.GetKeyDown(KeyCode.C) && canSlide == true)
+        {
             canSlide = false;
-         
+            timeStarted = true;
+            currentSpeed = Mathf.Lerp(basespeed * 1.5f, basespeed * 0.6f, t / slideTime);
         }
         if (Input.GetKeyUp(KeyCode.C))
         {
             canSlide = true;
             currentSpeed = basespeed;
         }
-        
+
+
     }
 
+  
+    /*
     void MovementSlide()
     {
-            slideDistance = Mathf.Lerp(1, 0.2f, Time.deltaTime * 2);
+        if (!timeStarted)
+        {
+            timeElapsed = Time.time;
+        }
+        float t =+ Time.time - timeElapsed;
+        
+
+            slideDistance = Mathf.Lerp(1, 0.2f, t / slideTime);
             currentSpeed = basespeed * (slideValue.Evaluate(slideDistance) * slideMultiplier);
-    }
+        timeStarted = true;
+    }*/
 }
 
 
