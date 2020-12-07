b33ee8295419bc9c404a061125b704ed2d309464
carlhaeggman <carl.haeggman02@gmail.com>
2020-12-07 10:15:55 +0100

    SakerSomSkjuterOchSnörrar

--- GymnaiseArbete/Assets/Scripts/Player/PlayerWeapSstm.cs
+++ GymnaiseArbete/Assets/Scripts/Player/PlayerWeapSstm.cs
@@ -74,7 +74,6 @@ public class PlayerWeapSstm : MonoBehaviour
                     crntWeapon = weapon2;
                     UpdateInfo(weapon2);
                 }
-
             }
         }
     }
